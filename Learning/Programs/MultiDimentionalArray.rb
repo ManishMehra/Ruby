930b0b3e10e888bd9ea9fa73d25ca125b5a6b859
Manish Mehra <manish.mehra@wipro.com>
2015-07-14 17:14:47 +0530

    Create MultiDimentionalArray.rb

---
+++ Learning/Programs/MultiDimentionalArray.rb
@@ -0,0 +1,16 @@
+C = ["1234",345666666666444444444333335,1276]
+string_array = [["ada","qwer","ger"],["frea","kel","sed"]]
+
+#puts C[1][2]
+#puts C[0][2]
+
+d = [[1,2],[3,4],[1,2]]
+puts string_array
+puts 
+string_array.each {|x| x.each {|y| puts y} }
+
+
+$end
+sleep 30
+
+#<Enumerator:0x2572d28>
