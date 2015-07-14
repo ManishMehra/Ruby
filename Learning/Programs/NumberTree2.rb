d65329164f9a0a29c24329d05008328a9cf872d2
Manish Mehra <ManishMehra@users.noreply.github.com>
2015-07-14 22:24:14 +0530

    Rename NumberTree.rb to NumberTree2.rb

---
+++ Learning/Programs/NumberTree2.rb
@@ -0,0 +1,46 @@
+i=1
+puts "enter count of rows in number tree"
+q=gets.to_i
+while (i<=q)
+	j=1
+	while (j<=i)
+		print j 
+		j=j+1
+	end
+	print "\n"
+	i=i+1
+end
+sleep 1
+
+puts"\n"
+puts "New Numeric tree will appear here"
+
+
+i=1
+puts "enter count of rows in number tree"
+q=gets.to_i
+while (i<=q)
+	j=i
+	while (j>0)
+		print j 
+		j=j-1
+	end
+	print "\n"
+	i=i+1
+end
+sleep 1
+
+
+i=1
+puts "enter count of rows in number tree"
+q=gets.to_i
+while (i<=q)
+	j=i
+	while (j>0)
+		print j 
+		j=j-1
+	end
+	print "\n"
+	i=i+1
+end
+sleep 1
